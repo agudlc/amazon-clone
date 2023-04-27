cdc6318364c7b7fd1e9f82d4e973945de1ea31ad
agudlc <agu.dlc97@gmail.com>
2023-04-27 14:18:10 -0300

    stripe implementation finished

--- api/src/stripe/stripe.service.ts
+++ api/src/stripe/stripe.service.ts
@@ -20,7 +20,7 @@ export class StripeService {
       0,
     );
     return this.stripe.paymentIntents.create({
-      amount: totalPrice * 100, // cents
+      amount: +totalPrice.toFixed(2) * 100, // cents
       currency: 'usd',
       payment_method_types: ['card'],
     });
--- client/src/features/products/components/Payment.component.tsx
+++ client/src/features/products/components/Payment.component.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import {
 	Elements,
@@ -6,6 +6,7 @@ import {
 	useStripe,
 	useElements,
 } from "@stripe/react-stripe-js";
+import axios from "axios";
 import { useAppDispatch, useAppSelector } from "../../../hooks/redux/hooks";
 import { resetCart } from "../productSlice";
 
@@ -29,12 +30,70 @@ const PaymentComponent = () => {
 		dispatch(resetCart);
 	}, []);
 
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (totalQty === 0) return;
+		if (!stripe || !elements) return;
+
+		const cardEl = elements.getElement(CardElement);
+
+		setIsProcessing(true);
+
+		try {
+			const res = await axios.post(
+				`${import.meta.env.VITE_REACT_APP_BASE_API}/stripe`,
+				{ cart }
+			);
+			const { client_secret: clientSecret } = res.data;
+			const { paymentIntent } = await stripe.confirmCardPayment(
+				clientSecret,
+				{
+					payment_method: {
+						card: cardEl!,
+					},
+				}
+			);
+			if (!paymentIntent) {
+				setPaymentStatus("Paiment failed!");
+			} else {
+				setPaymentStatus(paymentIntent.status);
+			}
+		} catch (err) {
+			console.error(err);
+			setPaymentStatus("Payment failed!");
+		}
+		setIsProcessing(false);
+	};
+
 	return (
 		<div style={{ fontSize: "20px" }}>
-			<form id="paymentForm">
+			<form onSubmit={handleSubmit} id="paymentForm">
 				<label htmlFor="card-element">Place order</label>
 				<CardElement id="card-element" />
-				{!isProcessing && <button style={{}}>Pay</button>}
+				{!isProcessing && (
+					<button
+						style={{
+							marginTop: "16px",
+							height: "31px",
+							backgroundColor: "#f0c14b",
+							color: "black",
+							display: "flex",
+							fontWeight: 600,
+							fontSize: "20px",
+							padding: "24px",
+							justifyContent: "center",
+							alignContent: "center",
+							cursor: "pointer",
+							width: "100%",
+						}}
+					>
+						Pay
+					</button>
+				)}
+				{isProcessing && <div>Processing...</div>}
+				{!isProcessing && paymentStatus && (
+					<div>Status: {paymentStatus}</div>
+				)}
 			</form>
 		</div>
 	);
